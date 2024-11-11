54e83c52fe53e0172ac3a6365c35cd574feda937
batonya-freaky <renatasavitskaya0509@gmail.com>
2024-11-11 22:31:03 +0100

    .

--- Assets/Script/TrunkManager.cs
+++ Assets/Script/TrunkManager.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrunkManager : MonoBehaviour
+public class trunkManager : MonoBehaviour
 {
+
+    public GameObject TrunkPrefab;
+    public GameObject branchLeftPrefab;
+    public GameObject branchRightPrefab;
     // Start is called before the first frame update
     void Start()
     {
