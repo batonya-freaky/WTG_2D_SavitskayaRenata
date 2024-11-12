0db52e74ee28ad9bb3a4865be4a013186d432998
batonya-freaky <renatasavitskaya0509@gmail.com>
2024-11-12 20:24:39 +0100

    .

---
+++ Assets/Script/Tap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        
+
+    // Update is called once per frame
+    void Update()
+        {
+
+        }
+
+    Debug.Log("Tap");
+}
+}
+
